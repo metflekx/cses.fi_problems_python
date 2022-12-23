50f7efbaf0597f928da9be9799a932ec653a9fc3
metflekx <dogfuelx@gmail.com>
2022-12-24 00:47:12 +0300

    added cses3

---
+++ cses3.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+int main()
+{
+	//gets input
+	std::string input;//to store input
+	//gets the input, stores it into variable
+	std::cin >> input;
+
+	/*makes sure input[0] != previous
+	since we don't have 'z' in DNA sequence */
+	char previous = 'z';
+
+	//to keep track of how many consecutive matches
+	int count_match = 0;
+
+	//to store the highest repeat
+	int highest = 0;
+
+	//iterates over all chars of input
+	for(int i = 0; i < input.length(); i++)
+	{
+		//if i == previous
+		if(input[i] == previous)
+		{
+			count_match++; // increments with each match
+
+			//updates highest if count count_match hits new record
+			if(count_match > highest)
+				highest = count_match;
+		}
+		else
+		{
+			if(count_match != 0)
+			{
+			/*this runs after a row finishes.*/
+
+				count_match = 0;//restart the counter 
+			}
+
+			//store input[i] - 1
+			previous = input[i];
+		}	
+	}
+
+	printf("%d\n", highest + 1);
+	return 0;
+}
